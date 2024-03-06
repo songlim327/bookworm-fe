78b042aa36d289cf20ec9fc7960138eb0a694d42
Song Lim <songlim327@gmail.com>
2024-03-06 11:46:44 +0800

    refactor: remove unused svelte store

--- src/store/bookworm.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const queries = writable([]);
-export const result = writable([]);
-export const chats = writable(["Hi, how can I help you today?"]);
